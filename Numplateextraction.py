d4f5700ccbd02def2f6c2d7946a7ba7fcc93bdea
Ismam Hussain Khan <ishmam.hussain.khan07@gmail.com>
2020-06-13 16:57:35 +0600

    Add files via upload
    Code for extracting number plate from car image

---
+++ Numplateextraction.py
@@ -0,0 +1,82 @@
+#import liberies
+
+import cv2
+import imutils
+import numpy as np
+import pytesseract
+from PIL import Image
+import matplotlib.pyplot as plt
+
+
+#Resize and gray scaling and dge detection
+img = cv2.imread('tiiis.jpg',cv2.IMREAD_COLOR)#insert file and image name
+
+img = cv2.resize(img, (620,480) )#resize the image
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #convert to grey scale
+gray = cv2.bilateralFilter(gray, 11, 17, 17) #Blur to reduce noise
+edged = cv2.Canny(gray, 30, 200) #Perform Edge detection
+
+cnts = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:10]
+screenCnt = None
+implot=plt.imshow(img)
+plt.show()
+implot=plt.imshow(gray)
+plt.show()
+implot=plt.imshow(edged)
+plt.show()
+
+#detection of contours
+for c in cnts:
+ 
+ peri = cv2.arcLength(c, True)
+ approx = cv2.approxPolyDP(c, 0.018 * peri, True)
+ 
+ 
+ if len(approx) == 4:
+  screenCnt = approx
+  break
+if screenCnt is None:
+  detected = 0
+  print ("No contour detected")
+else:
+  detected = 1
+
+if detected == 1:
+ cv2.drawContours(img, [screenCnt], -1, (0, 255, 0), 3)
+
+imgplot= plt.imshow(img)
+plt.show()
+imgplot= plt.imshow(edged)
+plt.show()
+
+
+
+#MAsking the imgae
+mask = np.zeros(gray.shape,np.uint8)
+new_image = cv2.drawContours(mask,[screenCnt],0,255,-1,)
+new_image = cv2.bitwise_and(img,img,mask=mask)
+imgplot= plt.imshow(new_image)
+plt.show()
+
+#Segmented the licence plate
+(x, y) = np.where(mask == 255)
+(topx, topy) = (np.min(x), np.min(y))
+(bottomx, bottomy) = (np.max(x), np.max(y))
+Cropped = gray[topx:bottomx+1, topy:bottomy+1]
+imgplot= plt.imshow(Cropped)
+plt.show()
+
+#convert the gray scale image to real image
+Cropped = img[topx:bottomx+1, topy:bottomy+1]
+imgplot= plt.imshow(Cropped)
+plt.show()
+
+
+#store the image as a png formate
+Cropped = cv2.resize(Cropped, (620,480) )
+
+cv2.imwrite( 'cropped1.png', Cropped )
+
